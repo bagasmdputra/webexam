efe39d336ead336fe306220e1e5c9b26ca7d6373
Your Name <bagas_mdp@yahoo.co.id>
2017-07-17 13:07:40 +0700

    fix rrouting

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -13,7 +13,7 @@ class AdminLoginController extends Controller
     }
     public function showLoginForm()
     {
-      return view('auth.admin-login')
+      return view('auth.admin-login');
     }
 
     public function login()
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 Route::prefix('admin')->group(function(){
-  Route::get('/login', 'AdminLoginController@showLoginForm')->name('admin.login');
-  Route::post('/login', 'AdminLoginController@login')->name('admin.login.submit');
+  Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+  Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
   Route::get('/', 'AdminController@index')->name('admin.dashboard');
 });
