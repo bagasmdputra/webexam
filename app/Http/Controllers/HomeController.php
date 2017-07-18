a4d55c015943cb79411552fbece2417958932793
Your Name <bagas_mdp@yahoo.co.id>
2017-07-18 15:13:06 +0700

    auth login logout

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,8 +21,8 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        return view('home');
-    }
+     public function index()
+     {
+         return view('home');
+     }
 }
