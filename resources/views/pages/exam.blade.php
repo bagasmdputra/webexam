3ed22bf879e98ab5c6d51e9bb6b64e5de4993d1a
Rizal Diantoro <iniguerizal@gmail.com>
2017-08-23 06:50:48 +0700

    Working on exam

--- resources/views/pages/exam.blade.php
+++ resources/views/pages/exam.blade.php
@@ -10,10 +10,10 @@
 
   <script src="{{asset('js/jquery.js')}}"></script>
   <script type="text/javascript">
-    // jQuery.ajax({
-    //   type: "POST",
-    //   url: "/exam",
-    // });
+  // jQuery.ajax({
+  //   type: "POST",
+  //   url: "/exam",
+  // });
   // function initReload() {
   //   console.log("Masuk");
   // }
@@ -83,33 +83,37 @@
             <tr><td><button class="answered tooltip"><p class="tooltiptext">Answered</p></button></td><td><button class="back-to-grid tooltip"><p class="tooltiptext">Back to Grid</p></button></td></tr>
           </table>
 
-          <table>
-            <tr><h2 style="font-size:40px; color: #888888; font-family:'Quicksand'; position:relative;">Question {{ $question-> id_question }}</h2></tr>
-            <tr><h3 style="font-size:15px; color: #888888; font-family:'Quicksand'; margin-top: 5%;">{{ $question-> question }}</h3></tr>
-            <ul style="margin-top: 5%;">
+          <form class="" action="/exam" method="post">
+            {{ csrf_field() }}
+            <table>
+              <tr><h2 style="font-size:40px; color: #888888; font-family:'Quicksand'; position:relative;">Question {{ $question-> id_question }}</h2></tr>
+              <tr><h3 style="font-size:15px; color: #888888; font-family:'Quicksand'; margin-top: 5%;">{{ $question-> question }}</h3></tr>
+              <ul style="margin-top: 5%;">
+                <tr>
+                  <td><label><input type="radio" name="user_answer" value="{{ $question-> answer }}">{{ $question-> answer }}</label></td>
+                </tr>
+                <tr>
+                  <td><label><input type="radio" name="user_answer" value="{{ $question-> answer }}">{{ $question-> answer }}</label></td>
+                </tr>
+                <tr>
+                  <td><label><input type="radio" name="user_answer" value="{{ $question-> answer }}">{{ $question-> answer }}</label></td>
+                </tr>
+                <tr>
+                  <td><label><input type="radio" name="user_answer" value="{{ $question-> answer }}">{{ $question-> answer }}</label></td>
+                </tr>
+                <tr>
+                  <td><label><input type="radio" name="user_answer" value="{{ $question-> answer }}">{{ $question-> answer }}</label></td>
+                </tr>
+              </ul>
               <tr>
-                <td><label><input type="radio" name="q{{ $question-> id_question }}" value="{{ $question-> answer }}">{{ $question-> answer }}</label></td>
               </tr>
-              <tr>
-                <td><label><input type="radio" name="q{{ $question-> id_question }}" value="{{ $question-> answer }}">{{ $question-> answer }}</label></td>
-              </tr>
-              <tr>
-                <td><label><input type="radio" name="q{{ $question-> id_question }}" value="{{ $question-> answer }}">{{ $question-> answer }}</label></td>
-              </tr>
-              <tr>
-                <td><label><input type="radio" name="q{{ $question-> id_question }}" value="{{ $question-> answer }}">{{ $question-> answer }}</label></td>
-              </tr>
-              <tr>
-                <td><label><input type="radio" name="q{{ $question-> id_question }}" value="{{ $question-> answer }}">{{ $question-> answer }}</label></td>
-              </tr>
-            </ul>
-            <tr>
-            </tr>
-          </table>
+            </table>
 
-          <button class="prev-next "  style="float:right; display: inline-block; margin-left: 2%; margin-right: -10%; margin-top: 15%; " onclick="nextQuestion()">Next</button>
-          <button class="prev-next"  style="float:right; display: inline-block; margin-right: 3%; margin-top: 15%; " onclick="prevQuestion()">Prev</button>
-          <button class="end-exam" style="float:left; display: inline-block;  margin-left: 2%; margin-top: 15%;" onclick="prevQuestion()">End Exam</button>
+            <input type="hidden" name="" value="">
+            <button type="submit" class="prev-next "  style="float:right; display: inline-block; margin-left: 2%; margin-right: -10%; margin-top: 15%; " onclick="nextQuestion()">Next</button>
+            <button type="submit" class="prev-next"  style="float:right; display: inline-block; margin-right: 3%; margin-top: 15%; " onclick="prevQuestion()">Prev</button>
+            <button type="submit" class="end-exam" style="float:left; display: inline-block;  margin-left: 2%; margin-top: 15%;" onclick="prevQuestion()">End Exam</button>
+          </form>
         </div>
         @endforeach
       </div>
