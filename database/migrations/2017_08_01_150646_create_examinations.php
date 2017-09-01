8bb7ab1d89db441e4809c2405cdecc91bbd58a4b
Bagaskoro Meyca <bagas_mdp@yahoo.co.id>
2017-09-01 23:04:20 +0700

    hanya ini

--- database/migrations/2017_08_01_150646_create_examinations.php
+++ database/migrations/2017_08_01_150646_create_examinations.php
@@ -17,6 +17,7 @@ class CreateExaminations extends Migration
           $table->increments('id')->unsigned();
           $table->string('name',30);
           $table->string('url_name');
+          $table->smallInteger('type')->default(0);
       });
     }
 
