ce38abc7cff631f7201374d5494518e509bc4f79
rqhoirunnisa <dqhoirunnisa@gmail.com>
2017-09-03 12:56:51 +0700

    result + score udah

---
+++ resources/views/rumah.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.landing')
+
+@section('content')
+  <div class="welcome">
+    <div class="welcome-left"></div>
+
+    <div class="welcome-right">
+      <div class="register-form">
+
+      </div>
+    </div>
+  </div>
+@endsection
